49951c8897e3a615afd7ac4e47f1965ea5b1e3e3
root <root@ip-172-26-10-228.us-east-2.compute.internal>
2018-09-09 22:33:40 +0000

    Added more passport config and added web routes

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,34 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::domain('www.itplog.com')->group(function () { 
+  
+    // Landing Page Routes
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    // Registration Routes...
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+    
+    // Catch All Route
+    Route::any('{any}', function () {
+        abort(404);
+    })->where('any', '.*');
 
-Route::get('/', function () {
-    return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Authentication Web Routes
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    
+// Password Reset Routes
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
